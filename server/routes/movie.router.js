a7572701b4beb6cc0233a25868858279a19dd03e
Benji Copan <benji1220@gmail.com>
2021-07-25 15:39:03 -0500

    rewrote the movie.router POST query to allow for multiple genres

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -32,20 +32,34 @@ router.post('/', (req, res) => {
     const createdMovieId = result.rows[0].id
 
     // Now handle the genre reference
-    const insertMovieGenreQuery = `
-      INSERT INTO "movies_genres" ("movie_id", "genre_id")
-      VALUES  ($1, $2);
-      `
-      // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
-      pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
-        //Now that both are done, send back success!
-        res.sendStatus(201);
-      }).catch(err => {
-        // catch for second query
-        console.log(err);
-        res.sendStatus(500)
-      })
-
+    // groundwork for adding multiple genres: expect req.body.genre_id to be an array of genre_ids
+    if (req.body.genre_id.length = 0) {
+      //NO GENRES TO ADD
+      res.sendStatus(201);
+    } else {
+      // init query
+      let insertMovieGenreQuery = `
+        INSERT INTO "movies_genres" ("movie_id", "genre_id")
+        VALUES 
+      `;
+      // add ($1, $#) for each genre_id, to add all in one query while protecting against SQL injection
+      for (const index in req.body.genre_id) {
+        insertMovieGenreQuery += `($1, $${index+2}),`
+      }
+      // remove last character - an we only need "," between insert values - and add closing ";"
+      insertMovieGenreQuery = insertMovieGenreQuery.substring(0, insertMovieGenreQuery.length - 1);
+      insertMovieGenreQuery += `;`
+        // SECOND QUERY ADDS GENRE(S) FOR THAT NEW MOVIE
+        pool.query(insertMovieGenreQuery, [createdMovieId].concat(req.body.genre_id)).then(result => {
+          //Now that both are done, send back success!
+          res.sendStatus(201);
+        }).catch(err => {
+          // catch for second query
+          console.log(err);
+          res.sendStatus(500)
+        })
+    }
+    
 // Catch for first query
   }).catch(err => {
     console.log(err);
