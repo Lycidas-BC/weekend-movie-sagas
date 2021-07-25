7d4ba17dfd972f2feeb14d73833c3aa8c5df2c4d
Benji Copan <benji1220@gmail.com>
2021-07-25 15:44:04 -0500

    add post, put, and delete routes to genre.router - still need to write queries

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -9,7 +9,43 @@ router.get('/', (req, res) => {
       res.send(result.rows);
     })
     .catch(err => {
-      console.log('ERROR: Get all genres', err);
+      console.log('ERROR: GET all genres', err);
+      res.sendStatus(500)
+    })
+});
+
+router.post('/', (req, res) => {
+  const query = `SELECT * FROM "genres" ORDER BY "name" ASC`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: POST new genre', err);
+      res.sendStatus(500)
+    })
+});
+
+router.put('/:genreId', (req, res) => {
+  const query = `SELECT * FROM "genres" ORDER BY "name" ASC`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: PUT to edit existing genre by id', err);
+      res.sendStatus(500)
+    })
+});
+
+router.delete('/:genreId', (req, res) => {
+  const query = `SELECT * FROM "genres" ORDER BY "name" ASC`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: DELETE genre by id', err);
       res.sendStatus(500)
     })
 });
