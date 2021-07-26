89f14afabd6e61794d3181a5d6e8d3c0aec6acac
Benji Copan <benji1220@gmail.com>
2021-07-25 23:50:29 -0500

    get genres

--- src/components/EditMovie/EditMovie.jsx
+++ src/components/EditMovie/EditMovie.jsx
@@ -10,6 +10,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
 import './EditMovie.css'
 
 const useStyles = makeStyles((theme) => ({root: {flexGrow: 1},paper: {padding: theme.spacing(2), textAlign: "center", color: theme.palette.text.secondary, justifyContent: "center" }})); // materialUI stuff
@@ -20,7 +21,15 @@ function EditMovie() {
     const { movieId } = useParams();
     const dispatch = useDispatch();
     const movieDetails = useSelector(store => store.movieDetails);
+    const genres = useSelector(store => store.genres);
     const [localMovieDetails, setLocalMovieDetails] = useState(movieDetails);
+    const [editModeBooleans, setEditModeBooleans] = useState({
+        title: false,
+        poster: false,
+        description: false
+    });
+    
+    let localCopyInitialized = false;
 
     const saveEdits = () => {
         console.log(`in saveEdits`, movieId);
@@ -32,36 +41,49 @@ function EditMovie() {
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: {id: movieId} });
+        dispatch({ type: 'FETCH_GENRES' });
     }, []);
 
-    console.log("movie details:", movieDetails);
+    console.log("movie details:", movieDetails, "genres:", genres);
     return (
         <section>
             <h1>Movie Details</h1>
             <Grid item style={{height: "100%", width: "75%", margin: "auto" }}  id={movieDetails.id}>
                 <Paper className={classes.paper}>
-                    <h2 style={{ color: "black" }}> <EditIcon></EditIcon> {movieDetails.title}</h2>
-                    <div style={{ display: "flex", alignContent: "flex-start", margin: "auto", justifyContent: "center" }}>
-                        <EditIcon style={{color: "black", alignItems: "right" }}></EditIcon>
-                        <CardMedia
-                            style={{maxHeight: "40%", maxWidth: "40%", padding: "10% 7% 10% 7%", backgroundImage: "url(../../images/frame.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}
-                            className={movieDetails.title}
-                            component="img"
-                            alt={movieDetails.title}
-                            src={`../../${movieDetails.poster}`}
-                            title={movieDetails.title}
-                        />
-                    </div>
-                    <div style={{color: "black", textAlign: "left", margin: "20px"}}> <EditIcon></EditIcon> <b>Description:</b> {movieDetails.description}</div>
+                    {
+                        editModeBooleans.title ?
+                        <TextField></TextField> :
+                        <h2 style={{ color: "black" }}> <Button><EditIcon></EditIcon></Button> {movieDetails.title}</h2>
+                    }
+                    {
+                        editModeBooleans.poster ?
+                        <TextField></TextField> :
+                        <div style={{ display: "flex", alignContent: "flex-start", margin: "auto", justifyContent: "center" }}>
+                            <Button><EditIcon style={{color: "black", alignItems: "right" }}></EditIcon></Button>
+                            <CardMedia
+                                style={{maxHeight: "40%", maxWidth: "40%", padding: "10% 7% 10% 7%", backgroundImage: "url(../../images/frame.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}
+                                className={movieDetails.title}
+                                component="img"
+                                alt={movieDetails.title}
+                                src={`../../${movieDetails.poster}`}
+                                title={movieDetails.title}
+                            />
+                        </div>
+                    }
+                    {
+                        editModeBooleans.description ?
+                        <TextField></TextField> :
+                        <div style={{color: "black", textAlign: "left", margin: "20px"}}> <Button><EditIcon></EditIcon></Button> <b>Description:</b> {movieDetails.description}</div>
+                    }
                     <div style={{color: "black", textAlign: "left", margin: "20px"}}>
                         <b>Genres:</b>
                         <ul>
                             {movieDetails.genreList.map((genre, index) => {
                                 return (
-                                    <li key={index}>{genre} <DeleteIcon></DeleteIcon></li>
+                                    <li key={index}>{genre} <Button><DeleteIcon></DeleteIcon></Button></li>
                                 )
                             })}
-                            <li><AddIcon></AddIcon></li>
+                            <li><Button><AddIcon></AddIcon></Button></li>
                         </ul>
                     </div>
                     <br />
