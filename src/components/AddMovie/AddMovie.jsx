dfbb889492c6d72c2133d83ebab4bcbb4922beef
Benji Copan <benji1220@gmail.com>
2021-07-25 19:48:16 -0500

    created components and routes (not yet functional) for MovieDetails, AddMovie, EditMovie, and Administration screens. Figured outhow to use react router url params

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './AddMovie.css'
+
+function AddMovie() {
+    const dispatch = useDispatch();
+    // const movies = useSelector(store => store.movies);
+
+    return (
+        <main>
+            <h1>AddMovie</h1>
+            
+        </main>
+    );
+}
+
+export default AddMovie;
---
+++ src/components/EditMovie/EditMovie.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import './EditMovie.css'
+
+function EditMovie() {
+    const { movieId } = useParams();
+    const dispatch = useDispatch();
+    // const movies = useSelector(store => store.movies);
+
+    return (
+        <main>
+            <h1>EditMovie</h1>
+            
+        </main>
+    );
+}
+
+export default EditMovie;
